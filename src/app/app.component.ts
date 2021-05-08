1c32e420649d8651a0939e57a02f39f687574cc1
jiajunda <1007274033@qq.com>
2021-05-08 20:34:46 +0800

    init: ng hero-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hero-app';
+  title = 'hero-app-Jack';
 }
+// 组件 代码逻辑 root 组件
