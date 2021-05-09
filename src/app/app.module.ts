ba525fd99506009c139261133bbfe4853a46e1e2
jiajunda <1007274033@qq.com>
2021-05-09 11:44:50 +0800

    feat: 动态展示 当前选择的英雄信息

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,9 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { HeroListComponent } from './hero-list/hero-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroListComponent],
+  declarations: [AppComponent, HeroesComponent],
   // 对应全局引入
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // 装饰器 指定元数据
 import Hero from '../hero';
+import heroList from '../heroList';
 @Component({
   selector: 'app-heroes', // name => type
   templateUrl: './heroes.component.html',
@@ -8,8 +9,15 @@ import Hero from '../hero';
 })
 export class HeroesComponent implements OnInit {
   public hero: Hero = { name: 'JackWorld', age: 18, id: 1 };
+  public selectedHero: Hero;
 
+  public heroList = heroList;
   constructor() {}
+
+  changeHero(hero: Hero) {
+    this.selectedHero = hero;
+  }
+
   // 生命周期钩子 hook
   ngOnInit(): void {
     console.log(this.hero);
